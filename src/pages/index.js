66a0a53be259640444d030d37ac233ade42acab9
Ryotaro <k.ryoutaro.g@gmail.com>
2020-08-10 00:59:55 +0900

    blog post 一覧作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,38 @@
 import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+export const query = graphql`
+  query blogs {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "l")
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1 className="text-align-center">Posts</h1>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <article key={node.frontmatter.slug}>
+        <h2>
+          <Link to={`/${node.frontmatter.slug}`}>{node.frontmatter.title}</Link>
+        </h2>
+        <time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time>
+        <p>{node.excerpt}</p>
+      </article>
+    ))}
   </Layout>
 )
 
