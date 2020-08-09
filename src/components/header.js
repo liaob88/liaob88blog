a92adb0eeb2f71858c87b55a399e7077e0bf25fa
Ryotaro <k.ryoutaro.g@gmail.com>
2020-08-10 01:01:03 +0900

    各種文言とデザインの修正

--- src/components/header.js
+++ src/components/header.js
@@ -5,22 +5,25 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: "#eeebf1",
       marginBottom: `1.45rem`,
+      position: "fixed",
+      top: 0,
+      width: '100%'
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `0.6rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `inherit`,
             textDecoration: `none`,
           }}
         >
