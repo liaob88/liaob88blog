54c2fecf0996eaa95611f9ddc1b33dec43f4fd3f
Ryotaro <k.ryoutaro.g@gmail.com>
2020-08-10 01:01:03 +0900

    各種文言とデザインの修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `liao3deblog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@liao3`,
   },
   plugins: [
     {
