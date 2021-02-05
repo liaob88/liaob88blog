ae1cbb5ecdcd5ea13383551b1fce3656a00114d7
liaob88 <50452793+liaob88@users.noreply.github.com>
2021-02-05 14:16:04 +0900

    add: new-article-generator

---
+++ new_article_generator.sh
@@ -0,0 +1,27 @@
+# Article info
+echo 'Type new article title'
+read TITLE
+echo 'Type new article description'
+read DESCRIPTION
+echo 'Type new article slag'
+read SLAG
+echo 'Type new article tags'
+declare TAGS=()
+read TAGS
+NOW=`date "+%Y-%m-%d %H:%M"`
+
+# Generate new md file with writing the article info on the file
+echo 'In Progress...'
+cat <<EOF > "src/blogs/${TITLE}.md"
+---
+title: $TITLE
+description: $DESCRIPTION
+date: $NOW
+slug: $SLAG
+tags: [$TAGS]
+---
+##
+##
+##
+EOF
+echo 'Done! File created!'
