9d32710947e7e8bba551efed4e4abf7c347e12cf
liaob88 <50452793+liaob88@users.noreply.github.com>
2021-02-05 15:14:37 +0900

    fix: md ファイルの名前を slug から生成するように修正

--- new_article_generator.sh
+++ new_article_generator.sh
@@ -12,7 +12,7 @@ NOW=`date "+%Y-%m-%d %H:%M"`
 
 # Generate new md file with writing the article info on the file
 echo 'In Progress...'
-cat <<EOF > "src/blogs/${TITLE}.md"
+cat <<EOF > "src/blogs/${SLUG}.md"
 ---
 title: $TITLE
 description: $DESCRIPTION
