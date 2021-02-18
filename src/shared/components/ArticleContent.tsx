f87fd6d925d040fcf1484aae916d739dc21d6e29
liaob88 <50452793+liaob88@users.noreply.github.com>
2021-02-18 19:40:31 +0900

    fix: md 内容を含む記事内容を表示する部分を ArticleContent コンポーネントとして共通化

---
+++ src/shared/components/ArticleContent.tsx
@@ -0,0 +1,54 @@
+import moment from "moment"
+import React from "react"
+import styled from "styled-components"
+import { MarkdownRemarkFrontmatter } from "../../../gatsby-graphql"
+import { TagsWrapper } from "../Styles"
+import Tags from "./Tags"
+
+interface Props {
+  html: string
+  post: MarkdownRemarkFrontmatter
+}
+
+const ArticleContent: React.FC<Props> = ({ html, post }) => {
+  return (
+    <PostPageWrapper>
+      <time>作成日: {moment(post.date).format(`MM.DD.YYYY`)}</time>
+      <h1>{post.title}</h1>
+      <TagsWrapper $bottomSpace={true}>
+        <Tags tags={post.tags} />
+      </TagsWrapper>
+      <MarkdownContent dangerouslySetInnerHTML={{ __html: html }} />
+    </PostPageWrapper>
+  )
+}
+
+const PostPageWrapper = styled.div`
+  p,
+  li {
+    font-size: 0.9em;
+  }
+  padding: 0 1rem;
+  time {
+    display: block;
+    text-align: end;
+    margin-bottom: 1rem;
+  }
+`
+
+const MarkdownContent = styled.div`
+  border-bottom: 1px dotted;
+  h2 {
+    border-left: 5px solid coral;
+    padding-left: 15px;
+  }
+  h3 {
+    border-bottom: 5px double coral;
+    display: inline-block;
+  }
+  a {
+    color: inherit;
+  }
+`
+
+export default ArticleContent
